611c463b9f02628aacbd45e643a048fef5794f09
Claudia Parravano <01claudia.parravano@gmail.com>
2022-12-26 12:52:57 +0100

    init new project

---
+++ app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>E-Commerce Store</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
